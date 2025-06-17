e7ac80cf273d7dc3bccfad8e4593d14563727f07
aeaeaaee <yeung27h@yahoo.com.hk>
2025-06-17 19:05:20 +0800

    Ready to implement the next function (Car, public transit, walk route)

--- TravelMate/LocationManager.swift
+++ TravelMate/LocationManager.swift
@@ -11,17 +11,46 @@ import CoreLocation
 class LocationManager: NSObject, ObservableObject, CLLocationManagerDelegate {
     private let locationManager = CLLocationManager()
     @Published var location: CLLocation?
-
+    @Published var heading: CLHeading?
+    
     override init() {
         super.init()
         self.locationManager.delegate = self
         self.locationManager.desiredAccuracy = kCLLocationAccuracyBest
         self.locationManager.requestWhenInUseAuthorization()
         self.locationManager.startUpdatingLocation()
+        
+        // Start updating heading if available
+        if CLLocationManager.headingAvailable() {
+            self.locationManager.startUpdatingHeading()
+        } else {
+            print("Heading information is not available on this device.")
+        }
     }
-
+    
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         guard let location = locations.last else { return }
         self.location = location
+    }
+    
+    // Delegate method for heading updates
+    func locationManager(_ manager: CLLocationManager, didUpdateHeading newHeading: CLHeading) {
+        // We only want to receive heading updates if the accuracy is good
+        // A negative value for headingAccuracy indicates that the heading is invalid
+        print("DEBUG: didUpdateHeading called. True Heading: \(newHeading.trueHeading), Accuracy: \(newHeading.headingAccuracy)")
+        if newHeading.headingAccuracy >= 0 {
+            self.heading = newHeading
+        } else {
+            // Optionally, you can set heading to nil or handle the invalid heading case
+            // For now, we'll just print a message and not update if accuracy is poor
+            print("Received invalid heading update with accuracy: \(newHeading.headingAccuracy)")
+        }
+    }
+
+    // Delegate method for handling errors with heading updates
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print("LocationManager failed with error: \(error.localizedDescription)")
+        // You might want to handle specific errors, e.g., CLAuthorizationStatus.denied
+        // or if heading updates fail for some reason.
     }
 }
--- TravelMate/TravelMateApp.swift
+++ TravelMate/TravelMateApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct TravelMateApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            MapView()
         }
     }
 }
